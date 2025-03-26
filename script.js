1351019c90a2bcdd9684e99d0c0c25e5ffe3d3b1
Shekapuram lokesh chandra <144873300+lokeshshekapuram@users.noreply.github.com>
2025-03-26 19:06:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 200) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
